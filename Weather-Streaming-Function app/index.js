140706741abeb54d3c7e6282143b523cfc8c78ca
ANUJ SHARMA <75572517+Anuj2656@users.noreply.github.com>
2026-05-05 15:47:56 +0530

    Delete Weather-Streaming-Function app directory

--- Weather-Streaming-Function app/index.js
+++
@@ -1,8 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.setup({
-    enableHttpStream: true,
-});
-
-// Import your function so it gets registered
-require('./functions/function_app.js');
